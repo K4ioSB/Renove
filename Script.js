f801d0346d4004591f8f2ab075b2c87baf370368
K4ioSB <ccainho11@gmail.com>
2023-10-18 18:56:20 -0300

    Barra Lateral Mobile

---
+++ Script.js
@@ -0,0 +1,47 @@
+const logo = document.querySelector(".logoRenove");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  // Define um ponto onde a imagem deve parar de diminuir (por exemplo, 200 pixels)
+  const stopPoint = 35;
+
+  // Calcula a escala com base na posição de rolagem
+  const scale = Math.max(1 - (scrollY / 100), 0.7); // Ajuste conforme necessário
+
+  // Calcula a posição vertical com base na posição de rolagem
+  const translateY = Math.min(scrollY / 3, stopPoint);
+
+  // Aplica a transformação de escala e posição vertical à imagem
+  logo.style.transform = `scale(${scale}) translateY(-${translateY}px)`;
+})
+
+
+function toggleMenu(){
+ const menuMobile = document.getElementById("menuMobile")
+
+ if(menuMobile.className === "menuMobile-active"){
+    menuMobile.className = "menuMobile"
+ }else{
+    menuMobile.className = "menuMobile-active"
+ }
+}
+
+function toggleOpcoesSolucoes(){
+const Opcoes = document.getElementById("opcoesSolucoesMobile")
+
+if(Opcoes.className === "opcoesSolucoesMobile-active"){
+    Opcoes.className = "opcoesSolucoesMobile"
+ }else{
+    Opcoes.className = "opcoesSolucoesMobile-active"
+ }
+}
+function toggleOpcoesSetores(){
+   const Opcoes = document.getElementById("opcoesSetoresMobile")
+   
+   if(Opcoes.className === "opcoesSetoresMobile-active"){
+       Opcoes.className = "opcoesSetoresMobile"
+    }else{
+       Opcoes.className = "opcoesSetoresMobile-active"
+    }
+   }
